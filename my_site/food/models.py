5fc53b661875ff5a114ce0b3e637f05f77737819
abdhulla-k <abdhullak149@gmail.com>
2022-06-27 14:39:40 +0530

    learne about models and migrations and how to create it

--- my_site/food/models.py
+++ my_site/food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Item( models.Model ):
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField( max_length= 200 )
+    itm_desc = models.CharField( max_length= 200 )
+    itm_price = models.IntegerField()
