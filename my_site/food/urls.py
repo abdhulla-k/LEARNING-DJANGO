b286e59d13d742c484fa08c5db7825b8733be0e8
abdhulla-k <abdhullak149@gmail.com>
2022-06-28 15:20:36 +0530

    learned about templates in django

--- my_site/food/urls.py
+++ my_site/food/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'food'   # name spacing. add this name to urls of this app
 urlpatterns = [
     path( '', views.index, name = 'index' ),
+    path( '<int:item_id>/', views.detail, name = 'detail' ),
     path( 'item/', views.item, name = 'item' ),
     # path( 'head/', views.head, name = 'head' )
 ]
--- my_site/food/views.py
+++ my_site/food/views.py
@@ -1,13 +1,27 @@
+from re import template
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 
 def index( request ):
-    return HttpResponse( 'Hello World' )
+    item_details = Item.objects.all()
+    context = {
+        'item_details': item_details
+    }
+    return render( request, 'food/index.html', context )
 
 def item( request ):
     return HttpResponse( "This is an item" )
+
+def detail( request, item_id ):
+    item = Item.objects.get( pk = item_id )
+    context = {
+        'item': item
+    }
+    return render( request, 'food/detail.html', context )
 
 # def head( request ):
 #     return HttpResponse( '<h1>This is the heading</h1>' )
