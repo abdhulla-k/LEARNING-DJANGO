4a3409806391f37dde0cb70d4dd34b935e2dedd5
abdhulla-k <abdhullak149@gmail.com>
2022-06-30 21:03:51 +0530

    created delete item functionality

--- my_site/food/urls.py
+++ my_site/food/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     # add new_item
     path( 'add/', views.create_item, name = 'create_item' ),
     # edit item
-    path( 'update/<int:id>/', views.update_item, name = 'update_item' )
+    path( 'update/<int:id>/', views.update_item, name = 'update_item' ),
+    # delete an item
+    path( 'delete/<int:id>/', views.delete_item, name = 'delete_item' )
 ]
