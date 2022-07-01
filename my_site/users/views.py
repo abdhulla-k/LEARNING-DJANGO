268e42166e6fbf6b9b3c460cd7dc44d3b9f76e42
abdhulla-k <abdhullak149@gmail.com>
2022-07-01 11:24:39 +0530

    added email field to signup form

--- my_site/users/views.py
+++ my_site/users/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
 
 # register user
 def register( request ):
     if request.method == 'POST':
-        form = UserCreationForm( request.POST )
+        form = RegisterForm( request.POST )
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get( username )
             messages.success( request, f'Welcome {username}, your account is successfully created!' )
             return redirect( 'food:index' )
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render( request, 'users/register.html', { 'form' : form })
