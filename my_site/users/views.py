9493d428895844d849df33024c69cd5a2a90140c
abdhulla-k <abdhullak149@gmail.com>
2022-07-01 11:02:22 +0530

    created user registration or signup functionality

--- my_site/users/views.py
+++ my_site/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+
+# register user
+def register( request ):
+    if request.method == 'POST':
+        form = UserCreationForm( request.POST )
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get( username )
+            messages.success( request, f'Welcome {username}, your account is successfully created!' )
+            return redirect( 'food:index' )
+    else:
+        form = UserCreationForm()
+    return render( request, 'users/register.html', { 'form' : form })
