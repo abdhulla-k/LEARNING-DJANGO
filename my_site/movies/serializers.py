bd33c435c3792646d251d1a858554c79583707fc
abdhulla-k <abdhullak149@gmail.com>
2022-07-13 16:33:54 +0530

    learned about serializers, created a serializer

---
+++ my_site/movies/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import MoviesData
+
+# using model serializer
+class ModelSerializer( serializers.ModelSerializer ):
+    class Meta:
+        model = MoviesData
+        fields = [ 'id', 'name', 'duration', 'rating']
