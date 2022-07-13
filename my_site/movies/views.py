b30edad824346d4471e331205bfbdc80c2ddb87e
abdhulla-k <abdhullak149@gmail.com>
2022-07-13 16:34:32 +0530

    created view and urls to access the serializer

--- my_site/movies/views.py
+++ my_site/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import MoviesData
+from rest_framework import viewsets
+from .serializers import ModelSerializer
 
 # Create your views here.
+
+class MovieViewSet( viewsets.ModelViewSet ):
+    queryset = MoviesData.objects.all()
+    serializer_class = ModelSerializer
